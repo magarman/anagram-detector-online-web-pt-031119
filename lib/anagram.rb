8aa614d9f1dcc95d48b64555431c9844185e9dbb
Mari Garman <garman.mariana@gmail.com>
2019-04-12 01:30:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,4 @@
 class Anagram
-<<<<<<< HEAD
   attr_accessor :word
   
 def initialize(word)
@@ -10,17 +9,4 @@ def match(words)
   words.select {|w| w.split("").sort == @word.split("").sort}
 end
   
-=======
-  attr_accessor :name
-  
-  def initialize(word)
-  
-end
-
-def match
-  
-end
-  
-  
->>>>>>> c058ac3827133d91c2677629d98c3f7f29be2ee0
 end
